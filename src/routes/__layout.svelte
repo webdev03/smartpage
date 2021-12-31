945add4615b31efb2329d849a35b00c4932c1501
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-01-01 12:05:15 +1300

    remove focus

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,7 @@
   import "$lib/tailwind.css";
 </script>
 
-<div class="!focus:outline-sky-300 w-screen min-h-screen dark:bg-gray-800 bg-gray-100 dark:text-gray-100 text-gray-900">
+<div class="w-screen min-h-screen dark:bg-gray-800 bg-gray-100 dark:text-gray-100 text-gray-900">
   <div class="p-2">
     <slot />
   </div>
