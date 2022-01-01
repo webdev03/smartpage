eec9e87c8133f7fb88f9bd4b448268b694fd11a6
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-01-01 20:35:43 +1300

    add ability to add quicklinks

--- src/lib/components/Quicklinks.svelte
+++ src/lib/components/Quicklinks.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
   import PencilSquare from "$lib/bootstrap-icons/pencil-square.svelte";
+  import Plus from "$lib/bootstrap-icons/plus.svelte";
   export let quicklinks;
   export let evalChange = () => {};
   let editing = false;
+  const createQuicklink = () => {
+    quicklinks.push({ name: "My Quicklink", link: "https://example.com" });
+    window.localStorage.setItem("quicklinks", JSON.stringify(quicklinks));
+    quicklinks = quicklinks;
+  }
 </script>
 
 <h1 class="inline mt-2 font-bold text-2xl">Quick Links</h1> 
 <button class="ml-2" on:click={() => editing = !editing}><PencilSquare /></button>
 <div class="flex flex-row flex-wrap row-span-3 space-x-2 flex-1 max-w-4xl overflow-scroll">
   <div />
-  {#each quicklinks as link}
-    {#if editing}
+  {#if editing}
+    {#each quicklinks as link}
     <div class="hover:font-semibold transition-all">
       <div
         class="inline-block mt-2 p-3 w-48 min-h-96 rounded-md bg-gray-200 dark:bg-gray-900 dark:text-white text-gray-900"
@@ -19,14 +25,19 @@
         <input type="text" class="w-36 bg-transparent" bind:value={link.link} on:input={evalChange}>
       </div>
     </div>
-    {:else}
-    <a href={link.link} class="hover:font-semibold transition-all">
-      <div
-        class="inline-block mt-2 p-3 w-36 min-h-96 rounded-md bg-gray-200 dark:bg-gray-900 dark:text-white text-gray-900"
-      >
-        {link.name}
-      </div>
-    </a>
-    {/if}
   {/each}
+  <button class="motion-safe:hover:animate-pulse" on:click={createQuicklink}><Plus /></button>
+  
+  {:else}
+    {#each quicklinks as link}
+  <a href={link.link} class="hover:font-semibold transition-all">
+    <div
+      class="inline-block mt-2 p-3 w-36 min-h-96 rounded-md bg-gray-200 dark:bg-gray-900 dark:text-white text-gray-900"
+    >
+      {link.name}
+    </div>
+  </a>
+  {/each}
+  
+  {/if}
 </div>
