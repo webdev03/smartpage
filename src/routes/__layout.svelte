22a4bb38cabe2e7a3dee686423839141794255d2
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-01-03 17:08:51 +1300

    format code

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,7 +6,9 @@
   <title>Smartpage | The customisable start page</title>
 </svelte:head>
 
-<div class="grid items-center justify-center w-screen min-h-screen dark:bg-gray-800 bg-gray-100 dark:text-gray-100 text-gray-900">
+<div
+  class="grid items-center justify-center w-screen min-h-screen dark:bg-gray-800 bg-gray-100 dark:text-gray-100 text-gray-900"
+>
   <div class="p-2">
     <slot />
   </div>
